30ac5c9530483493b00941e512f2e6ca9aa68e1c
Chigozie Nwobi <Chigozie.Nwobi757@yahoo.com>
2024-01-18 14:28:09 +0100

    Added add todo components

---
+++ src/components/add_todo/AddTodo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./AddTodo.scss";
+
+export default function AddTodo() {
+  return (
+    <div className="addTodo">
+      <input
+        className="addTodo__input"
+        type="text"
+        placeholder="Add your todo"
+      />
+      <button className="addTodo__button">+</button>
+    </div>
+  );
+}
